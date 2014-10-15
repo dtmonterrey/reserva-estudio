fc0e7bef213ad69c1cdcf63728df2f9b101ed4e1
Lucas <lucashenrique0207@hotmail.com>
2014-10-15 07:30:08 -0300

    view indisponibilidade

---
+++ views/indisponibilidade/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Indisponibilidades';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="indisponibilidade-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Indisponibilidade', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'id_estudio',
+            'inicio',
+            'fim',
+            'repetir',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
