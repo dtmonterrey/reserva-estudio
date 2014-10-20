e42df1c21f7290b628558be2965fa94505c7327e
Ricardo Gonçalves <ricardomdl@gmail.com>
2014-10-20 18:41:00 +0100

    Integração do widget fullcalendar
    Não esquecer de fazer "composer update" para actualizar as dependências.

--- views/reserva/index.php
+++ views/reserva/index.php
@@ -33,5 +33,36 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
+    
+    
+    <div>
+<?php 
+
+$events = array();
+  //Testing
+  $Event = new \yii2fullcalendar\models\Event();
+  $Event->id = 1;
+  $Event->title = 'Testing';
+  $Event->start = date('Y-m-d\TH:m:s\Z');
+  $events[] = $Event;
+
+  $Event = new \yii2fullcalendar\models\Event();
+  $Event->id = 2;
+  $Event->title = 'Testing';
+  $Event->start = date('Y-m-d\TH:m:s\Z',strtotime('tomorrow 6am'));
+  $events[] = $Event;
+
+
+  ?>
+
+  <?= \yii2fullcalendar\yii2fullcalendar::widget(array(
+      	'events'=> $events
+  ));
+?>
+
+
+</div>
+    
+    
 
 </div>
