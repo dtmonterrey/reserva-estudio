6c7b16e47d9019e1b910738d8710788974b5a742
Ricardo Gonçalves <ricardomdl@gmail.com>
2014-10-30 16:36:30 +0000

    Teste do pedido GET para gravar na BD
    Usado em Reserva

--- views/reserva/index.php
+++ views/reserva/index.php
@@ -14,12 +14,12 @@ $this->params['breadcrumbs'][] = $this->title;
  <div class="reserva-index"> 
 
     <h1><?= Html::encode($this->title) ?></h1>
-
+<!--
     <p>
-        <?= Html::a('Create Reserva', ['create'], ['class' => 'btn btn-success']) ?>
+         //Html::a('Create Reserva', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
+     /*GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
@@ -34,73 +34,65 @@ $this->params['breadcrumbs'][] = $this->title;
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
-    
+    ]); */?>
+    -->
     
     <div> 
 <?php 
 
 $events = array();
   //Testing
-  $Event = new \yii2fullcalendar\models\Event();
+  /*$Event = new \yii2fullcalendar\models\Event();
   $Event->id = 1;
   $Event->title = 'Testing';
+  $Event->className = 'eventoAprovado';
   $Event->start = date('Y-m-d\TH:m:s\Z');
-  $events[] = $Event;
+  $events[] = $Event;*/
   
-  
- 
-  
-  $JsSelect= "function(start, end) {	var title = prompt('Event Title:');
-		 									var eventData;
-		 									if (title) {
-		 						    		eventData = {
-		 						                title: title,
-		 						                start: start,
-		 						                end: end
-		 						            };
-											$('#reservaCal').fullCalendar('renderEvent', eventData, true); // stick? = true
-											}
-											$('#reservaCal').fullCalendar('unselect');
-											}";
-        
+  $JsSelect= "function(start, end) 
+  				{/*var title = prompt('Event Title:');*/
+					var title = 'UserLogin';
+		 			var eventData;
+		 			if (title) {
+		 				eventData = {
+		 					title: title,
+							className: 'eventoPendente',
+		 					start: start,
+		 					end: end
+		 				};
+						$('#reservaCal').fullCalendar('renderEvent', eventData, true); // stick? = true
+					}
+					$('#reservaCal').fullCalendar('unselect');
+					$.get('index.php?r=reserva/teste', {xispe: eventData.title});
+					}";
+					
+  $colunas = "{
+			agendaWeek: 'ddd - DD/MM'
+		}";
 
-		
-  		 $JsSelect= "function(start, end) {	var title = prompt('Event Title:');
-		 									var eventData;
-		 									if (title) {
-		 						    		eventData = {
-		 						                title: title,
-		 						                start: start,
-		 						                end: end
-		 						            };
-											$('#reservaCal').fullCalendar('renderEvent', eventData, true); // stick? = true
-											}
-											$('#reservaCal').fullCalendar('unselect');
-											}";
+  $titulo = "{
+			agendaWeek: 'LL'
+		}";					
   		 
 
   $options = array();
   $options = [
   		'lang'=>'pt',
   		'defaultView' =>'agendaWeek',
-  		'axisFormat'=> 'HH:mm',
-  		'agendaWeek'=>':ddd - DD/MM',
-  		'agendaWeek'=> ' LL',
-  		'minTime'=> '09:00:00',
-  		'maxTime'=> '19:00:00',
-  		'defaultView'=> 'agendaWeek',
-  		'firstDay'=>'1' ,
-  		'Duration'=> '01:00:00',
-  		'height'=>513,
-  		'allDaySlot'=> false,
-  		'editable'=> true,
+  		'axisFormat' => 'HH:mm',
+		'columnFormat' => new JsExpression($colunas),
+		'titleFormat' => new JsExpression($titulo),
+  		'minTime' => '09:00:00',
+  		'maxTime' => '19:00:00',
+  		'firstDay' =>'1',
+		'snapDuration' => '01:00:00',
+  		'height' => 513,
+  		'allDaySlot' => false,
+  		'editable' => true,
   		'selectable' => true,
   		'selectHelper'=> true,
         'select' => new JsExpression($JsSelect)
   ];
-
-  
   
   ?>
 
